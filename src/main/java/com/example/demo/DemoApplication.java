affcee31d9902de68d3cd0cdf908c50d7f00b61c
ali_aus <dev@investmentor.com.au>
2021-10-22 10:28:15 +0530

    functional style controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,12 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
@@ -17,12 +19,37 @@ import java.time.Instant;
 @SpringBootApplication
 public class DemoApplication {
 
+	@Bean
+	RouterFunction<ServerResponse> routes(GreetingService greetingService) {
+
+
+		return RouterFunctions.route()
+//				.POST()
+				.GET("/greeting/{name}",
+						r -> ServerResponse.ok()
+								.body(greetingService.greet(new GreetingRequest(r.pathVariable("name"))),
+										GreetingResponse.class))
+								.build();
+		/*return RouterFunctions.route()
+				.GET("/greeting/{name}", new HandlerFunction<ServerResponse>() {
+					@Override
+					public Mono<ServerResponse> handle(ServerRequest request) {
+						GreetingRequest greetingRequest = new GreetingRequest(request.pathVariable("name"));
+						Mono<GreetingResponse> greet = greetingService.greet(greetingRequest);
+						return ServerResponse.ok().body(greet, GreetingResponse.class);
+					}
+				})
+				.build();*/
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
 
+
+
+/*
 @RestController
 @RequiredArgsConstructor
 class GreetingRestController {
@@ -33,7 +60,7 @@ class GreetingRestController {
 	Mono<GreetingResponse> greet(@PathVariable String name) {
 		return this.greetingService.greet(new GreetingRequest(name));
 	}
-}
+}*/
 
 @Service
 class GreetingService {
