938a992b9c3154658d1f33db0d38e826bf1e8a74
ali_aus <dev@investmentor.com.au>
2021-10-22 12:01:28 +0530

    infite stream push from server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,14 +7,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -22,12 +27,15 @@ public class DemoApplication {
 	@Bean
 	RouterFunction<ServerResponse> routes(GreetingService greetingService) {
 
-
 		return RouterFunctions.route()
 //				.POST()
+				.GET("/greetings/{name}",
+						r -> ServerResponse.ok()
+								.contentType(MediaType.TEXT_EVENT_STREAM)
+				.body(greetingService.greetMany(new GreetingRequest(r.pathVariable("name"))), GreetingResponse.class))
 				.GET("/greeting/{name}",
 						r -> ServerResponse.ok()
-								.body(greetingService.greet(new GreetingRequest(r.pathVariable("name"))),
+								.body(greetingService.greetOnce(new GreetingRequest(r.pathVariable("name"))),
 										GreetingResponse.class))
 								.build();
 		/*return RouterFunctions.route()
@@ -69,7 +77,13 @@ class GreetingService {
 		return new GreetingResponse("Hello " + name + " @ " + Instant.now());
 	}
 
-	Mono<GreetingResponse> greet(GreetingRequest request) {
+	Flux<GreetingResponse> greetMany(GreetingRequest request) {
+		return Flux
+				.fromStream(Stream.generate(() -> greet(request.getName())))
+				.delayElements(Duration.ofSeconds(1));
+	}
+
+	Mono<GreetingResponse> greetOnce(GreetingRequest request) {
 		return Mono.just(greet(request.getName()));
 	}
 }
